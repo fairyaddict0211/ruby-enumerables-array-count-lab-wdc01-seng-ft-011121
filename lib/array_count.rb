84384d4d9ade140bc85b82ea666287d24b324b3a
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-02 20:43:20 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,7 @@ def count_strings(array)
   array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
   
   array.count do |string|
-    strings = ""
+    string .is_a? String
 end
 
 def count_empty_strings(array)
